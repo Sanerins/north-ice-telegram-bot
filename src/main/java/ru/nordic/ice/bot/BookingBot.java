9097293824b16e8a3253157c7fa820f7e01005b0
Aleksandr Kosnitskii <ASKosnitskiy@gmail.com>
2025-09-06 00:29:36 +0300

    Remove unnecessary imports

--- src/main/java/ru/nordic/ice/bot/BookingBot.java
+++ src/main/java/ru/nordic/ice/bot/BookingBot.java
@@ -1,6 +1,5 @@
 package ru.nordic.ice.bot;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
--- src/main/java/ru/nordic/ice/bot/dao/UserData.java
+++ src/main/java/ru/nordic/ice/bot/dao/UserData.java
@@ -4,7 +4,6 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
 import java.time.Instant;
 
 public class UserData implements Serializable {
